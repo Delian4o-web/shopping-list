3e45188908c7cc27bae5962f9a7b6505861d7d6a
Delian4o <dgeorgiev@jhb.dvt.co.za>
2020-03-26 18:24:07 +0200

    Add Imports

--- shoppinglist/ngrx-shopping-list/src/app/app.module.ts
+++ shoppinglist/ngrx-shopping-list/src/app/app.module.ts
@@ -1,16 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { StoreModule } from "@ngrx/store";
+import { AppComponent } from "./app.component";
+import { ShoppingReducer } from "../app/store/reducers/shopping.reducer";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    StoreModule.forRoot({
+      shopping: ShoppingReducer
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
