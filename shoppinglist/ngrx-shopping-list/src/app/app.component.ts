d8e32515749f6cd47d8d5fa7caf4a3b920a3dc20
Delian4o <dgeorgiev@jhb.dvt.co.za>
2020-03-26 18:32:53 +0200

    Add State to be selected

--- shoppinglist/ngrx-shopping-list/src/app/app.component.ts
+++ shoppinglist/ngrx-shopping-list/src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
-
+import { Component } from "@angular/core";
+import { Store } from "@ngrx/store";
+import { Observable } from "rxjs";
+import { ShoppingItem } from "../app/store/models/shopping-item-model";
+import { AppState } from "../app/store/models/app-state.model";
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  title = 'ngrx-shopping-list';
+  shoppingItems: Observable<Array<ShoppingItem>>;
+
+  constructor(private store: Store<AppState>) {}
+
+  ngOnInit() {
+    this.shoppingItems = this.store.select(store => store.shopping);
+  }
 }
