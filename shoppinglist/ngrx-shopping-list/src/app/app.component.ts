04267730067a5372b0fe7f6fdbecd34307dc913d
Delian4o <dgeorgiev@jhb.dvt.co.za>
2020-03-26 18:37:35 +0200

    Add New item feature

--- shoppinglist/ngrx-shopping-list/src/app/app.component.ts
+++ shoppinglist/ngrx-shopping-list/src/app/app.component.ts
@@ -1,8 +1,11 @@
 import { Component } from "@angular/core";
 import { Store } from "@ngrx/store";
 import { Observable } from "rxjs";
+import { v4 as uuid } from "uuid";
+
 import { ShoppingItem } from "../app/store/models/shopping-item-model";
 import { AppState } from "../app/store/models/app-state.model";
+import { AddItemAction } from "../app/store/actions/shopping.actions";
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
@@ -10,10 +13,17 @@ import { AppState } from "../app/store/models/app-state.model";
 })
 export class AppComponent {
   shoppingItems: Observable<Array<ShoppingItem>>;
+  newShoppingItem: ShoppingItem = { id: "", name: "" };
 
   constructor(private store: Store<AppState>) {}
 
   ngOnInit() {
     this.shoppingItems = this.store.select(store => store.shopping);
+  }
+
+  addItem() {
+    this.newShoppingItem.id = uuid(); //generate unique id
+    this.store.dispatch(new AddItemAction(this.newShoppingItem));
+    this.newShoppingItem = { id: "", name: "" };
   }
 }
