bda4787dbcf48946160683519506f0e25b804e9b
Delian4o <dgeorgiev@jhb.dvt.co.za>
2020-04-03 09:23:27 +0200

    Add PWA

--- shoppinglist/ngrx-shopping-list/src/app/app.module.ts
+++ shoppinglist/ngrx-shopping-list/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from "./app.component";
 import { ShoppingReducer } from "../app/store/reducers/shopping.reducer";
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from '../environments/environment';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   declarations: [AppComponent],
@@ -15,7 +16,8 @@ import { environment } from '../environments/environment';
     StoreModule.forRoot({
       shopping: ShoppingReducer
     }),
-    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production })
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
   bootstrap: [AppComponent]
